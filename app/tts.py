4d8bd8424a25b6c077a1a099c3e9482c5accea14
HuangWei <huangwei.a180285@gmail.com>
2023-08-05 12:11:29 +0800

    support asyncio for tts

--- app/tts.py
+++ app/tts.py
@@ -2,23 +2,29 @@ import asyncio, time
 from .messages_handler import popMessagesQueue
 from .stats import appendDelay
 import pyttsx3
+import concurrent.futures
+import functools
 
+ttsEngine = None
 
-ttsEngine = pyttsx3.init()  # object creation
-ttsEngine.setProperty('volume', 1.0)    # setting up volume level  between 0 and 1
-voices = ttsEngine.getProperty('voices')       #getting details of current voice
-for voice in voices:
-    print(voice.id)
-    print(voice.name)
-    # print(voice.languages)
-ttsEngine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
-ttsEngine.setProperty('rate', 1000)     # setting up new voice rate
-ttsEngine.say("tts 初始化成功")
-ttsEngine.runAndWait()
+def tts_init():
+    global ttsEngine
+    ttsEngine = pyttsx3.init()  # object creation
+    ttsEngine.setProperty('volume', 1.0)    # setting up volume level  between 0 and 1
+    voices = ttsEngine.getProperty('voices')       #getting details of current voice
+    for voice in voices:
+        print(voice.id)
+        print(voice.name)
+    ttsEngine.setProperty('voice', voices[0].id)   #changing index, changes voices. 1 for female
+    ttsEngine.setProperty('rate', 1000)     # setting up new voice rate
+    ttsEngine.say("tts 初始化成功")
+    ttsEngine.runAndWait()
 
 
 def tts(text):
     global ttsEngine
+    if ttsEngine == None:
+        tts_init()
     ttsEngine.say(text)
     ttsEngine.runAndWait()
 
@@ -39,14 +45,18 @@ def messagesToText(msg):
         return f"欢迎{msg['uname']}进入直播间"
 
 async def ttsTask():
-    while True:
-        msg = popMessagesQueue()
-        if msg == None:
-            await asyncio.sleep(0.1)
-            continue
-        appendDelay(time.time() - msg['time'])
-        text = messagesToText(msg)
-        tts(text)
+    loop = asyncio.get_running_loop()
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        await loop.run_in_executor(pool, tts_init)
+
+        while True:
+            msg = popMessagesQueue()
+            if msg == None:
+                await asyncio.sleep(0.1)
+                continue
+            appendDelay(time.time() - msg['time'])
+            text = messagesToText(msg)
+            await loop.run_in_executor(pool, functools.partial(tts, text))
 
 if __name__ == '__main__':
     pass
