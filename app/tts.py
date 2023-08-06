8caea6b0b76af282f76a7200066f62604ce16845
HuangWei <huangwei.a180285@gmail.com>
2023-08-06 16:20:33 +0800

    add winrt.windows.media.speechsynthesis for tts

--- app/tts.py
+++ app/tts.py
@@ -1,45 +1,73 @@
 import asyncio, time, re
+
 from .messages_handler import popMessagesQueue
 from .stats import appendDelay
 from .config import getJsonConfig
 from .logger import timeLog
-import concurrent.futures
-import win32com.client as wincl
+import winrt.windows.media.speechsynthesis as speechsynthesis
+import winrt.windows.media.playback as playback
 
-ttsEngine = None
 lastRate = None
 lastVolume = None
-
-def init():
-    global ttsEngine
-    dynamicConfig = getJsonConfig()['tts']
-    ttsEngine = wincl.Dispatch("SAPI.SpVoice")
-    vcs = ttsEngine.GetVoices()
-    targetVoice = None
-    for i in range(0, vcs.Count):
-        item = vcs.Item(i)
-        name = item.GetAttribute("Name")
-        timeLog(f'[TTS] Found tts voices: {name}')
-        if dynamicConfig['voice'] in name:
-            targetVoice = item
-    timeLog(f'[TTS] Use voice: {targetVoice.GetAttribute("Name")} as target voice')
-    ttsEngine.Voice = targetVoice
-    tts("TTS模块初始化成功")
+synthesizer = None
+media_player = None
 
 
-def tts(text):
+def syncWithConfig():
     global ttsEngine, lastRate, lastVolume
     ttsConfig = getJsonConfig()['tts']
     if lastVolume != ttsConfig['volume']:
-        ttsEngine.Volume = ttsConfig['volume']
         lastVolume = ttsConfig['volume']
+        synthesizer.options.audio_volume = lastVolume / 100.0
     if lastRate != ttsConfig['rate']:
-        ttsEngine.Rate = ttsConfig['rate']
         lastRate = ttsConfig['rate']
+        # This value can range from 0.5 (half the default rate) to 6.0 (6x the default rate), inclusive.
+        # The default value is 1.0 (the "normal" speaking rate for the current voice).
+        synthesizer.options.speaking_rate = 0.5 + (lastRate / 100.0) * 5.5
+
+async def init():
+    global synthesizer, media_player
+    synthesizer = speechsynthesis.SpeechSynthesizer()
+    media_player = playback.MediaPlayer()
+
+    voices = synthesizer.get_all_voices()
+    dynamicConfig = getJsonConfig()['tts']
+    targetVoice = None
+    for index, voice in enumerate(voices):
+        if dynamicConfig['voice'] in voice.display_name:
+            targetVoice = voice
+            break
+
+    if targetVoice != None:
+        timeLog(f'[TTS] Use voice: {targetVoice.display_name} ({targetVoice.language})"')
+        synthesizer.voice = targetVoice
+    else:
+        voice = synthesizer.voice
+        timeLog(f'[TTS] Use default voice: {voice.display_name} ({voice.language})"')
+
+    syncWithConfig()
+    await tts("TTS模块初始化成功")
+
+
+async def tts(text):
+    syncWithConfig()
+    global synthesizer, media_player
+    ttsConfig = getJsonConfig()['tts']
+
     # 支持日语
     if ttsConfig['japanese']['enable']:
         text = re.sub(r'[\u3040-\u309F\u30A0-\u30FF]+', r'<lang langid="411">\g<0></lang>', text)
-    ttsEngine.Speak('<lang langid="804">' + text + '</lang>')
+    ssml = '<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="zh-CN"><voice xml:lang="zh-CN">'\
+           + text + '</voice></speak>'
+    # Synthesize text to a stream
+    stream = await synthesizer.synthesize_ssml_to_stream_async(ssml)
+    # stream = await synthesizer.synthesize_text_to_stream_async(text)
+
+    media_player.set_stream_source(stream)
+    media_player.play()
+    sound_seconds = stream.size / (32.0 * 1024)  # 32 KB/s
+    await asyncio.sleep(sound_seconds)
+
 
 def messagesToText(msg):
     if msg['type'] == 'danmu':
@@ -58,14 +86,12 @@ def messagesToText(msg):
         return f"欢迎{msg['uname']}进入直播间"
 
 async def ttsTask():
-    init()
-    loop = asyncio.get_running_loop()
-    with concurrent.futures.ThreadPoolExecutor() as pool:
-        while True:
-            msg = popMessagesQueue()
-            if msg == None:
-                await asyncio.sleep(0.1)
-                continue
-            appendDelay(time.time() - msg['time'])
-            text = messagesToText(msg)
-            await loop.run_in_executor(pool, tts, text)
+    await init()
+    while True:
+        msg = popMessagesQueue()
+        if msg == None:
+            await asyncio.sleep(0.1)
+            continue
+        appendDelay(time.time() - msg['time'])
+        text = messagesToText(msg)
+        await tts(text)
