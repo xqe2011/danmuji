590e0abb723f3d1ca209621112d5d872b420cb30
xqe2011 <gz923553148@gmail.com>
2025-06-15 21:12:12 +0800

    chore: 删除server模块，已迁移到fuwuji项目 chore: 删除深度学习相关模块

--- server/app/config.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-HTTP_SERVER_PASSWORD = os.getenv('HTTP_SERVER_PASSWORD')
--- server/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-time=$(date "+%Y%m%d-%H%M%S")
-filename=$time.txt
-python3 -m app.main $1 2>&1 | tee "logs/$filename"
